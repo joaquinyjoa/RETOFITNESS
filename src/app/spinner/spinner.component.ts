51f812f83734428157873b54129f2a4acab3b40a
joaquinyjoa <joaquinalfredogreco@gmail.comgit config --global user.email joaquinalfredogreco@gmail.com>
2026-02-03 00:14:02 -0300

    Característica: Refactorizar componente spinner para incluir overlay y gestión de scroll al mostrar/ocultar.

--- src/app/spinner/spinner.component.ts
+++ src/app/spinner/spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,5 +8,15 @@ import { CommonModule } from '@angular/common';
   templateUrl: './spinner.component.html',
   styleUrl: './spinner.component.scss'
 })
-export class SpinnerComponent {
+export class SpinnerComponent implements OnInit, OnDestroy {
+  
+  ngOnInit() {
+    // Bloquear scroll cuando el spinner aparece
+    document.body.style.overflow = 'hidden';
+  }
+
+  ngOnDestroy() {
+    // Restaurar scroll cuando el spinner desaparece
+    document.body.style.overflow = '';
+  }
 }
